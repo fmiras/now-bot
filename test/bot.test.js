611ff6de6718b7c97ad0346f71be31cfcdf3a7f0
Federico Miras <miras.federico@gmail.com>
2017-05-13 19:31:16 -0300

    Create first test for deployments listing

---
+++ test/bot.test.js
@@ -0,0 +1,26 @@
+import { ask, __RewireAPI__ as BotRewireApi } from '../lib/bot'
+
+test('List 2 real deployments', async () => {
+  const nowMock = {}
+  nowMock.getDeployments = async () => [
+    {
+      uid: '7Npest0z1zW5QVFfNDBId4BW',
+      name: 'my-project',
+      url: 'my-project-wtbxvynenu.now.sh',
+      created: '1460801613968'
+    },
+    {
+      uid: 'dOgCUIoovYiYmXbrLX0h9qDk',
+      name: 'project-b',
+      url: 'project-b-iipihlfrpa.now.sh',
+      created: '1462738579605'
+    }
+  ]
+
+  BotRewireApi.__Rewire__('now', nowMock)
+
+  const answer = await ask(undefined, 'List all my deployments')
+  expect(answer.attachment.payload.buttons.length).toBe(2)
+  expect(answer.attachment.payload.buttons[0].url)
+    .toBe('my-project-wtbxvynenu.now.sh')
+})
