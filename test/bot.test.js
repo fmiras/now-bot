1eca00999b3e3901510ddfb0a9288a05653e8373
Federico Miras <miras.federico@gmail.com>
2017-05-15 17:21:36 -0300

    Write some tests and console logging sentences

--- test/bot.test.js
+++ test/bot.test.js
@@ -21,5 +21,40 @@ test('Try to list a non-deployment account', async () => {
   BotRewireApi.__Rewire__('NowClient', emptyNow)
 
   const answer = await ask(undefined, 'List all my deployments')
-  expect(answer.text).toBe('There are no deployments for this account!')
+  expect(answer.text).toBe(messages.NO_DEPLOYMENTS)
+})
+
+test('Do anything without authentication', async () => {
+  BotRewireApi.__Rewire__('getToken', async () => undefined)
+
+  const answer = await ask(undefined, 'List all my deployments')
+  expect(answer.text).toBe(messages.MISSING_TOKEN)
+})
+
+test('First authentication for user with valid token', async () => {
+  BotRewireApi.__Rewire__('getToken', async () => undefined)
+  BotRewireApi.__Rewire__('validateToken', async () => true)
+  BotRewireApi.__Rewire__('newUser', async () => true)
+
+  const answer = await ask(undefined, '24-digits-text-as-token!')
+  expect(answer.text).toBe(messages.VALID_TOKEN)
+})
+
+test('First authentication for user with invalid token', async () => {
+  BotRewireApi.__Rewire__('getToken', async () => undefined)
+  BotRewireApi.__Rewire__('validateToken', async () => false)
+
+  const answer = await ask(undefined, '24-digits-text-as-token!')
+  expect(answer.text).toBe(messages.INVALID_TOKEN)
+})
+
+test('First authentication with firebase unavailable', async () => {
+  BotRewireApi.__Rewire__('getToken', async () => undefined)
+  BotRewireApi.__Rewire__('validateToken', async () => true)
+  BotRewireApi.__Rewire__('newUser', async () => {
+    throw new Error('firebase not working')
+  })
+
+  const answer = await ask(undefined, '24-digits-text-as-token!')
+  expect(answer.text).toBe(messages.SERVICE_UNAVAILABLE)
 })
