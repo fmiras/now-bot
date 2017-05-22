b58e87e334a1fe49ce12cc179914ddcd4ad05238
Federico Miras <miras.federico@gmail.com>
2017-05-22 19:49:31 -0300

    Change firebase with mongo and fix tests

--- test/user.test.js
+++ test/user.test.js
@@ -2,18 +2,16 @@ import {getToken, __RewireAPI__ as UserRewireApi} from '../lib/user'
 
 const TOKEN_STRING = '24-digits-text-as-token!'
 
-const userSnapshotMock = {
-  val: () => { return { token: TOKEN_STRING } }
-}
+const userMock = { token: TOKEN_STRING }
 
 test('Successfully get existing token of a user', async () => {
-  UserRewireApi.__Rewire__('getUserSnapshot', async () => userSnapshotMock)
+  UserRewireApi.__Rewire__('getUser', async () => userMock)
   const token = await getToken('sender-id')
   expect(token).toBe(TOKEN_STRING)
 })
 
 test('Token of user not found', async () => {
-  UserRewireApi.__Rewire__('getUserSnapshot', async () => {
+  UserRewireApi.__Rewire__('getUser', async () => {
     throw new Error('user not found')
   })
   const token = await getToken('sender-id')
